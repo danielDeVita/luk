01d18d969e834b4887944911f3a1891e3ac930ce
Daniel De Vita <danielitodevita@gmail.com>
2026-01-20 22:06:59 -0300

    fix explicit graphql types for price reduction and husky config

--- backend/src/raffles/entities/price-reduction.entity.ts
+++ backend/src/raffles/entities/price-reduction.entity.ts
@@ -20,12 +20,12 @@ export class PriceReduction {
   @Field()
   ticketsVendidosAlMomento!: number;
 
-  @Field({ nullable: true })
+  @Field(() => Boolean, { nullable: true })
   aceptada?: boolean | null;
 
-  @Field({ nullable: true })
+  @Field(() => Date, { nullable: true })
   fechaRespuesta?: Date | null;
 
-  @Field({ nullable: true })
+  @Field(() => String, { nullable: true })
   raffleTitulo?: string;
 }
